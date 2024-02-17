2ac09d0af23e42ac136a313764bed151a471a0e4
Akbar <k.akbar1101@gmail.com>
2024-02-17 17:38:14 -0500

    fixing bags

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -2,5 +2,5 @@ import {ConfigService} from '@nestjs/config'
 import {JwtModuleOptions} from '@nestjs/jwt'
 
 export const getJWTConfig = async (ConfigService: ConfigService): Promise<JwtModuleOptions> => ({
-    secret: ConfigService.get('JWT_SECRET')
+    secret: ConfigService.get('JWT_SECRET') || process.env.JWT_SECRET
 })
