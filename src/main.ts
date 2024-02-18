ccac5933c9755e4051d6952bb54d0353bd48f543
Akbar <k.akbar1101@gmail.com>
2024-02-17 19:16:27 -0500

    fixING CORS err

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT);
 }
 bootstrap();
