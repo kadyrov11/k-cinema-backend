0946f0518a256bcc564ca3b57af37be359e0705e
Akbar <k.akbar1101@gmail.com>
2024-02-17 19:06:49 -0500

    fixed CORS err

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as process from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(process.env.PORT);
