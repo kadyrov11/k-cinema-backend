c96d45ae99ffd08a596158b8d9e262bbab3588ee
Akbar <k.akbar1101@gmail.com>
2024-02-25 17:22:24 -0500

    fix origin

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,24 @@ async function bootstrap() {
     ],
     methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS,PATCH',
     credentials: true,
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+    allowedHeaders: [
+      'Origin',
+      'Accept',
+      'Pragma',
+      'Referer',
+      'User-Agent',
+      'Content-Type',
+      'Cache-Control',
+      'Authentication',
+      'Accept-Encoding',
+      'Accept-Language',
+      'X-Requested-With',
+      'Access-control-allow-origin',
+      'Access-control-allow-methods',
+      'Access-control-allow-headers',
+      'Access-Control-Request-Headers',
+      'Access-control-allow-credentials',
+    ],
   });
 
   const port = process.env.PORT || 4000;
